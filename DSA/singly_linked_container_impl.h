bab0ba7dd75f7b63791847c0eab66431ee7d8f8e
Nick Huang <nickhuang1009@gmail.com>
2016-06-01 09:29:04 -0400

    Re-structure singly linked container class.

--- DSA/singly_linked_container_impl.h
+++ DSA/singly_linked_container_impl.h
@@ -12,8 +12,13 @@ namespace DSA
         {
             throw index_error("Index out of bounds.");
         }
-        node_ptr cur_node
-            = node_ptr(m_head);
+#ifdef SINGLY_LINKED_CONTAINER_TAIL_REFERENCE_FLAG
+        if (m_size - 1 == index)
+        {
+            return m_tail;
+        }
+#endif
+        node_ptr cur_node = m_head;
         for (size_t i = 0; i < index; i++)
         {
             cur_node = cur_node->next();
@@ -38,54 +43,178 @@ namespace DSA
         throw no_such_element("Cannot find such entry in the container.");
     }
 
+#ifdef SINGLY_LINKED_CONTAINER_TAIL_REFERENCE_FLAG
     template <class T>
-    void singly_linked_container<T>::insert_front(const T &entry)
+    singly_linked_container<T>::singly_linked_container()
+        : m_head(), m_tail() {}
+#else
+    template <class T>
+    singly_linked_container<T>::singly_linked_container()
+        : m_head() {}
+#endif
+
+    template <class T>
+    singly_linked_container<T>::~singly_linked_container() {}
+
+    template <class T>
+    bool singly_linked_container<T>::empty() const
     {
-        node_ptr new_node
-            = node_ptr(new s_node<T>(entry));
+        return (0 == m_size);
+    }
+
+    template <class T>
+    size_t singly_linked_container<T>::size() const
+    {
+        return m_size;
+    }
+
+    template <class T>
+    void singly_linked_container<T>::clear()
+    {
+        m_size = 0;
+        m_head.reset();
+        m_tail.reset();
+    }
+
+    template <class T>
+    void singly_linked_container<T>::push_front(const T &entry)
+    {
+        node_ptr new_node = node_ptr(new s_node<T>(entry));
         new_node->set_next(m_head);
-        m_head = std::move(new_node);
+        m_head = new_node;
+#ifdef SINGLY_LINKED_CONTAINER_TAIL_REFERENCE_FLAG
+        if (0 == m_size)
+        {
+            m_tail = new_node;
+        }
+#endif
         m_size++;
     }
 
     template <class T>
-    void singly_linked_container<T>::insert_entry(const size_t &index, const T &entry)
+    void singly_linked_container<T>::push_back(const T &entry)
+    {
+        node_ptr new_node = node_ptr(new s_node<T>(entry));
+#ifdef SINGLY_LINKED_CONTAINER_TAIL_REFERENCE_FLAG
+        m_tail ? m_tail->set_next(new_node) : m_head = new_node;
+        m_tail = new_node;
+        m_size++;
+#else
+        insert(m_size, entry);
+#endif
+    }
+
+    template <class T>
+    void singly_linked_container<T>::insert
+    (const size_t &index, const T &entry)
     {
         // Case when adding to the frong of the list
         if (0 == index)
         {
-            insert_front(entry);
+            push_front(entry);
         }
-        // Other cases.
+#ifdef SINGLY_LINKED_CONTAINER_TAIL_REFERENCE_FLAG
+        else if (m_size == index)
+        {
+            push_back(entry);
+        }
+#endif
         else
         {
-            node_ptr new_node
-                = node_ptr(new s_node<T>(entry));
-            node_ptr prev_node
-                = std::move(node_at(index - 1));
+            node_ptr new_node = node_ptr(new s_node<T>(entry));
+            node_ptr prev_node = node_at(index - 1);
             new_node->set_next(prev_node->next());
             prev_node->set_next(new_node);
             m_size++;
         }
     }
 
-#ifdef SINGLY_LINKED_CONTAINER_TAIL_REFERENCE_FLAG
+    // Remove and return the object at the front of the container.
     template <class T>
-    singly_linked_container<T>::s_linked_container()
-        : m_head(), m_tail() {}
-#else
-    template <class T>
-    singly_linked_container<T>::s_linked_container()
-        : m_head() {}
-#endif
-
-    template <class T>
-    singly_linked_container<T>::~s_linked_container() {}
-
-    template <class T>
-    void singly_linked_container<T>::clear()
+    T singly_linked_container<T>::pop_front()
     {
-        base_container<T>::clear();
+        node_ptr cur_node = node_at(index);
+        T cur_entry = cur_node->data();
+        m_head = cur_node->next();
+#ifdef SINGLY_LINKED_CONTAINER_TAIL_REFERENCE_FLAG
+        // Case when removing the last entry in the list.
+        if (!m_head)
+        {
+            m_tail.reset();
+        }
+#endif
+        m_size--;
+        return cur_entry;
+    }
+
+    // Remove and return the object at the end of the container.
+    template <class T>
+    T singly_linked_container<T>::pop_back()
+    {
+        return remove(m_size - 1);
+    }
+
+    // Remove a given entry from the container.
+    // Note that only one entry will be removed if there are duplicates.
+    // Throw no_such_element exception if the entry is not found.
+    template <class T>
+    void singly_linked_container<T>::remove(const T &entry)
+    {
+        if (0 != m_size)
+        {
+            // Check head.
+            if (entry == m_head->data())
+            {
+                pop_front();
+                return;
+            }
+#if defined(SINGLY_LINKED_CONTAINER_TAIL_REFERENCE_FLAG) \
+    && defined(SINGLY_LINKED_CONTAINER_UNSTABLE_INDEX_FLAG)
+            if (entry == m_tail->data())
+            {
+                pop_back();
+                return;
+            }
+#endif
+            node_ptr prev_node = m_head;
+            while (prev_node->next())
+        }
+        throw no_such_element("Cannot find such entry in the container.");
+    }
+
+    // Remove the entry at a given index.
+    // Throw index_error exception when index out of bounds.
+    template <class T>
+    T singly_linked_container<T>::remove(const size_t &index)
+    {
+        if (0 == index)
+        {
+            return pop_front();
+        }
+        else
+        {
+            node_ptr prev_node = node_at(index - 1);
+            node_ptr cur_node = prev_node->next();
+            T cur_entry = cur_node->data();
+            prev_node->set_next(cur_node->next());
+#ifdef SINGLY_LINKED_CONTAINER_TAIL_REFERENCE_FLAG
+            // Case when removing the last entry from the list.
+            if (!cur_node->next())
+            {
+                m_tail = prev_node;
+            }
+#endif
+            m_size++;
+            return cur_entry;
+        }
+    }
+
+    // Return the entry at a given position in the container.
+    // Throw index_error exception when index out of bounds.
+    template <class T>
+    T singly_linked_container<T>::at(const size_t &index) const
+    {
+        return node_at(index)->data();
     }
 
     // Get the index of a given entry.
@@ -93,20 +222,24 @@ namespace DSA
     template <class T>
     size_t singly_linked_container<T>::index_of(const T &entry) const
     {
+#if defined(SINGLY_LINKED_CONTAINER_TAIL_REFERENCE_FLAG) \
+    && defined(SINGLY_LINKED_CONTAINER_UNSTABLE_INDEX_FLAG)
+        if (entry == m_tail->data())
+        {
+            return m_size - 1;
+        }
+#endif
         size_t index = 0;
-        node_ptr cur_node = std::move(m_head);
-
+        node_ptr cur_node = m_head;
         while (cur_node)   // cur_node != nullptr;
         {
-            // Fix this:
             if (entry == cur_node->data())
             {
                 return index;
             }
             index++;
-            cur_node = std::move(cur_node->next());
+            cur_node = cur_node->next();
         }
-
         throw no_such_element("Cannot get the index of an entry that \
 is not in the list.");
     }
@@ -114,15 +247,22 @@ is not in the list.");
     template <class T>
     bool singly_linked_container<T>::contains(const T &entry) const
     {
-        try
+#ifdef SINGLY_LINKED_CONTAINER_TAIL_REFERENCE_FLAG
+        if (entry == m_tail->data())
         {
-            index_of(entry);
+            return true;
         }
-        catch (no_such_element exception)
+#endif
+        node_ptr cur_node = m_head;
+        while (cur_node)   // cur_node != nullptr;
         {
-            return false;
+            if (entry == cur_node->data())
+            {
+                return true;
+            }
+            cur_node = cur_node->next();
         }
-        return true;
+        return false;
     }
 }
 
